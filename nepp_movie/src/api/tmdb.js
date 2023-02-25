2b4365b7ac6fd9339bdb9b75bcb9c6606cd926b9
eunhee <hd96010@daum.net>
2023-02-25 16:31:48 +0900

    환경설정 파일 만들기
    .gitignore 파일에 env 추가하여 API 키값이 git에서 노출되지 않도록 하기

--- nepp_movie/src/api/tmdb.js
+++ nepp_movie/src/api/tmdb.js
@@ -4,7 +4,8 @@ import axios from "axios";
 export const tmdbAxios = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "d57203ff00ea90c015eca21638f4116e",
+    //api_key: "d57203ff00ea90c015eca21638f4116e",
+    api_key: process.env.REACT_APP_TMDB_API_KEY,
   },
 });
 
